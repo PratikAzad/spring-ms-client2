c76c05867fced160d2bab8e27cf33d989080963e
PratikAzad <pkazad23@gmail.com>
2018-10-01 13:06:53 +0530

    innitial commit

---
+++ src/main/java/com/example/springmsclient2/SpringMsClient2Application.java
@@ -0,0 +1,12 @@
+package com.example.springmsclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsClient2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMsClient2Application.class, args);
+	}
+}
